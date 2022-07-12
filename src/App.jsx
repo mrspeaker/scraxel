094eaa6e2bd8d4f5edc9736dba3b5ec2d5f711e2
Earle Castledine <earlec@21csw.com>
2022-07-12 14:58:19 +0800

    app has data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import "./App.css";
+import { useState } from "react";
 import Projects from "./Projects.jsx";
+import { default_data } from "./data/index.js";
 
-function App({ data }) {
+function App() {
+    const [data, setData] = useState(default_data());
+
+    const add_project = (name) => {
+        setData((d) => ({
+            ...d,
+            projects: [...d.projects, { name }],
+        }));
+    };
+
     return (
         <div className="App">
             <h1>Scraxel</h1>
+            <button onClick={() => add_project(window.prompt("name"))}>
+                add
+            </button>
             <Projects projects={data.projects} />
         </div>
     );
